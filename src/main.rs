523ad55beab3c7122d02e5a6fd3b1db6fb1bfff4
Scott Checko <schecko@sfu.ca>
2021-01-09 03:24:54 -0800

    adding more commands and triggers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate tokio;
 extern crate anyhow;
+extern crate rand;
 
 use twitchchat::{ 
     commands, connector, messages,
@@ -7,80 +8,51 @@ use twitchchat::{
     UserConfig,
 };
 use anyhow::Context as _; 
+use std::time::{ Duration, Instant };
+use rand::Rng;
 
-async fn connect(user_config: &UserConfig, channels: &[String]) -> anyhow::Result<AsyncRunner> {
-    // create a connector using ``tokio``, this connects to Twitch.
-    // you can provide a different address with `custom`
+const TRIGGERS: &[&str] = &[
+    "cpp",
+    "c++",
+    "lua",
+    "java",
+];
+
+const RESPONSES: &[&str] = &[
+    "Rust is bae",
+    "You Should have chosen rust instead",
+];
+
+const ADVICE: &[&str] = &[
+    "Don't forget to commit, feeder.",
+    "Bro you should take a break.",
+    "Eat tendies frequently.",
+];
+
+const PASSIVE_MESSAGES: bool = false;
+const TRIGGER_MESSAGES: bool = false; 
+const COMMAND_MESSAGES: bool = true;
+
+async fn connect(user_config: &UserConfig, channel: &str) -> anyhow::Result<AsyncRunner> {
     let connector = connector::tokio::ConnectorRustTls::twitch()?;
 
-    println!("we're connecting!");
-    // create a new runner. this is a provided async 'main loop'
-    // this method will block until you're ready
+    println!("Connecting...");
     let mut runner = AsyncRunner::connect(connector, user_config).await?;
-    println!("..and we're connected");
-
-    // and the identity Twitch gave you
-    println!("our identity: {:#?}", runner.identity);
-
-    for channel in channels {
-        // the runner itself has 'blocking' join/part to ensure you join/leave a channel.
-        // these two methods return whether the connection was closed early.
-        // we'll ignore it for this demo
-        println!("attempting to join '{}'", channel);
-        let _ = runner.join(&channel).await?;
-        println!("joined '{}'!", channel);
-    }
+    println!("..Connected, attempting to join channel '{}'", channel); 
+    let _ = runner.join(&channel).await?;
+    println!("joined '{}'!", channel); 
 
     Ok(runner)
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // create a user configuration
-    let user_config = get_user_config()?;
-    // get some channels to join from the environment
-    let channels = channels_to_join()?;
-    println!("{:?}", user_config);
-    println!("{:?}", channels);
+    let (user_config, channel) = get_config()?;
 
-    // connect and join the provided channels
-    let runner = connect(&user_config, &channels).await?;
-
-    // you can get a handle to shutdown the runner
-    let quit_handle = runner.quit_handle();
-
-    // you can get a clonable writer
-    let mut writer = runner.writer();
-
-    // spawn something off in the background that'll exit in 10 seconds
-    tokio::spawn({
-        let mut writer = writer.clone();
-        let channels = channels.clone();
-        async move {
-            println!("in 10 seconds we'll exit");
-            //tokio::time::delay_for(std::time::Duration::from_secs(10)).await;
-
-            // send one final message to all channels
-            for channel in channels {
-                let cmd = commands::privmsg(&channel, "goodbye, world");
-                writer.encode(cmd).await.unwrap();
-            }
-
-            println!("sending quit signal");
-            quit_handle.notify().await;
-        }
-    });
-
-    // you can encode all sorts of 'commands'
-    for channel in &channels {
-        writer
-            .encode(commands::privmsg(channel, "hello world!"))
-            .await?;
-    }
-
+    let runner = connect(&user_config, &channel).await?; 
     println!("starting main loop");
-    // your 'main loop'. you'll just call next_message() until you're done
-    main_loop(runner).await
+    let state = State::new(channel);
+    main_loop(state, runner).await
 }
 
 // some helpers for the demo
@@ -88,47 +60,89 @@ fn get_env_var(key: &str) -> anyhow::Result<String> {
     std::env::var(key).with_context(|| format!("please set `{}`", key))
 }
 
-pub fn get_user_config() -> anyhow::Result<twitchchat::UserConfig> {
+pub fn get_config() -> anyhow::Result<(twitchchat::UserConfig, String)> {
     let name = get_env_var("TWITCH_NAME")?;
     let token = get_env_var("TWITCH_TOKEN")?;
+    let channel = get_env_var("TWITCH_CHANNEL")?;
 
-    // you need a `UserConfig` to connect to Twitch
     let config = UserConfig::builder()
-        // the name of the associated twitch account
+        // twitch account name
         .name(name)
-        // and the provided OAuth token
+        // OAuth token
         .token(token)
-        // and enable all of the advanced message signaling from Twitch
         .enable_all_capabilities()
         .build()?;
 
-    Ok(config)
+    Ok((config, channel))
 }
 
-// channels can be either in the form of '#museun' or 'museun'. the crate will internally add the missing #
-pub fn channels_to_join() -> anyhow::Result<Vec<String>> {
-    let channels = get_env_var("TWITCH_CHANNEL")?
-        .split(',')
-        .map(ToString::to_string)
-        .collect();
-    Ok(channels)
+const PASSIVE_ADVICE_INTERVAL: Duration = Duration::from_secs(60 * 30); // 30min
+const BACKOFF_ADVICE_INTERVAL: Duration = Duration::from_secs(60 * 60 * 24); // 24h
+
+pub enum Mood {
+    Normal,
+    Backoff,
 }
 
-// a 'main loop'
-pub async fn main_loop(mut runner: AsyncRunner) -> anyhow::Result<()> {
+pub struct State { 
+    pub channel: String,
+    pub mood: Mood,
+    pub last_advice: Instant,
+    pub next_advice: Duration, 
+    pub dedup_message: bool,
+}
+
+impl State {
+    fn new(channel: String) -> Self {
+        State {
+            channel,
+            mood: Mood::Normal,
+            last_advice: Instant::now(),
+            next_advice: PASSIVE_ADVICE_INTERVAL,
+            dedup_message: false,
+        }
+    }
+
+    async fn send_message(&mut self, runner: &mut AsyncRunner, msg: &str) {
+        let mut writer = runner.writer();
+        let cmd = commands::privmsg(&self.channel, msg);
+        writer.encode(cmd).await.unwrap(); 
+
+        self.dedup_message = true;
+        self.last_advice = Instant::now();
+    }
+}
+
+pub async fn main_loop(mut state: State, mut runner: AsyncRunner) -> anyhow::Result<()> {
     loop {
         match runner.next_message().await? {
             // this is the parsed message -- across all channels (and notifications from Twitch)
             Status::Message(msg) => {
-                handle_message(msg).await;
+                handle_message(&mut state, &mut runner, msg).await;
             } 
             Status::Quit => {
-                println!("we signaled we wanted to quit");
+                println!("Quitting.");
                 break;
             }
             Status::Eof => {
-                println!("we got a 'normal' eof");
+                println!("Eof");
                 break;
+            }
+        }
+
+        if state.last_advice + state.next_advice > Instant::now() {
+            match state.mood {
+                Mood::Normal => {
+                    let mut rng = rand::thread_rng();
+                    let msg = ADVICE[rng.gen::<usize>() % ADVICE.len()];
+                    if PASSIVE_MESSAGES && !state.dedup_message {
+                        state.send_message(&mut runner, msg).await
+                    }
+                }
+                Mood::Backoff => {
+                    state.mood = Mood::Normal; 
+                    state.next_advice = PASSIVE_ADVICE_INTERVAL;
+                }
             }
         }
     }
@@ -136,23 +150,65 @@ pub async fn main_loop(mut runner: AsyncRunner) -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn handle_message(msg: messages::Commands<'_>) {
+async fn parse_command(state: &mut State, runner: &mut AsyncRunner, msg: &messages::Privmsg<'_>) -> anyhow::Result<()> {
+    if COMMAND_MESSAGES {
+        match msg.data() {
+            "fuckoff" | "fuck off" => {
+                state.send_message(runner, "No, you fuck off.").await;
+                return Ok(());
+            }
+            "!fuckoff" => {
+                state.send_message(runner, "Fine, I'll fuck off.").await; 
+                state.next_advice = BACKOFF_ADVICE_INTERVAL;
+                state.mood = Mood::Backoff;
+                return Ok(());
+            }
+            "!feed" => {
+                state.send_message(runner, "Mmm I love tendies.").await; 
+                return Ok(());
+            }
+            "!bot" => {
+                state.send_message(runner, "github.com/schecko/cynobot").await; 
+                return Ok(());
+            }
+            _ => {}
+        } 
+    }
+        
+    if TRIGGER_MESSAGES {
+        println!("scanning triggers");
+        for trigger in TRIGGERS {
+            if msg.data().contains(trigger) {
+                println!("found trigger");
+                let mut rng = rand::thread_rng();
+                let msg = RESPONSES[rng.gen::<usize>() % RESPONSES.len()];
+                state.send_message(runner, msg).await; 
+                break;
+            }
+        } 
+    }
+
+    Ok(())
+}
+
+async fn handle_message(state: &mut State, runner: &mut AsyncRunner, msg: messages::Commands<'_>) {
     use messages::Commands::*;
     match msg {
-        // This is the one users send to channels
-        Privmsg(msg) => println!("[{}] {}: {}", msg.channel(), msg.name(), msg.data()),
+        Privmsg(msg) => {
+            println!("[{}] {}: {}", msg.channel(), msg.name(), msg.data());
+            let _ = parse_command(state, runner, &msg).await.unwrap();
+            state.dedup_message = false;
+        },
 
-        // This one is special, if twitch adds any new message
-        // types, this will catch it until future releases of
-        // this crate add them.
+        // unimplemented features from crate twitchchat
         Raw(_) => {}
 
-        // These happen when you initially connect
+        // initial connection events
         IrcReady(_) => {}
         Ready(_) => {}
         Cap(_) => {}
 
-        // and a bunch of other messages you may be interested in
+        // other events
         ClearChat(_) => {}
         ClearMsg(_) => {}
         GlobalUserState(_) => {}
